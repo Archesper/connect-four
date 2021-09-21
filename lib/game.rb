532f4eaf4997b4bb9aedd2f5dafecaed79cdca18
Archesper <pk4nde@gmail.com>
2021-09-22 00:33:51 +0100

    Add Game#player_input

--- lib/game.rb
+++ lib/game.rb
@@ -8,6 +8,11 @@ class Game
     @board = Board.new
   end
 
+  def player_input
+    puts 'Pick the column you wanna drop a disk in ( From 1 to 7 )'
+    gets.chomp
+  end
+
   def verify_input(input)
     return input if input.match(/^[1-7]$/)
   end
@@ -19,5 +24,9 @@ class Game
     elsif player_disc_counts[0] < player_disc_counts[1]
       @players[0]
     end
+  end
+
+  def first_player
+    @players.sample
   end
 end
