0ff306e25b36bf359f7704a38f1500db24f6f5c6
Archesper <pk4nde@gmail.com>
2021-09-22 00:14:17 +0100

    Write tests for Game#current_player

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -15,22 +15,56 @@ describe Game do
         verified_input = game.verify_input(valid_input)
         expect(verified_input).to eql(valid_input)
       end
+    end
 
-      context 'when input is invalid it returns nil' do
-        it 'returns nil for out of range numbers' do
-          invalid_number = '17'
-          verified_input = game.verify_input(invalid_number)
-          expect(verified_input).to be_nil
-        end
+    context 'when input is invalid it returns nil' do
+      it 'returns nil for out of range numbers' do
+        invalid_number = '17'
+        verified_input = game.verify_input(invalid_number)
+        expect(verified_input).to be_nil
+      end
 
-        it 'returns nil for symbols or letters' do
-          letter = 'a'
-          symbol = '$'
-          verified_input1 = game.verify_input(letter)
-          verified_input2 = game.verify_input(symbol)
-          expect(verified_input1).to be_nil
-          expect(verified_input2).to be_nil
-        end
+      it 'returns nil for symbols or letters' do
+        letter = 'a'
+        symbol = '$'
+        verified_input1 = game.verify_input(letter)
+        verified_input2 = game.verify_input(symbol)
+        expect(verified_input1).to be_nil
+        expect(verified_input2).to be_nil
+      end
+    end
+  end
+
+  describe '#current_player' do
+    context 'when player1 has put more discs than player2' do
+      before do
+        allow(game.instance_variable_get(:@board)).to receive(:count_disc).with('dummy 1').and_return(7)
+        allow(game.instance_variable_get(:@board)).to receive(:count_disc).with('dummy 2').and_return(5)
+      end
+      it 'returns player2' do
+        current_player = game.current_player
+        expect(current_player).to equal(player2)
+      end
+    end
+    context 'when player1 has put less discs than player2' do
+      before do
+        allow(game.instance_variable_get(:@board)).to receive(:count_disc).with('dummy 1').and_return(5)
+        allow(game.instance_variable_get(:@board)).to receive(:count_disc).with('dummy 2').and_return(7)
+      end
+      it 'returns player1' do
+        current_player = game.current_player
+        expect(current_player).to equal(player1)
+      end
+    end
+
+    context 'at start of the game, when both players at 0 disks' do
+      before do
+        allow(game.instance_variable_get(:@board)).to receive(:count_disc).with('dummy 1').and_return(0)
+        allow(game.instance_variable_get(:@board)).to receive(:count_disc).with('dummy 2').and_return(0)
+      end
+      it 'returns nil' do
+        current_player = game.current_player
+        expect(current_player).to be_nil
       end
     end
   end
