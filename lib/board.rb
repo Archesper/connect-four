b3c25f8004785aaf7f079381d265f9061957c7b6
Archesper <pk4nde@gmail.com>
2021-09-21 20:27:49 +0100

    Add Tokens module and Board#to_s

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: false
+
+require './lib/tokens'
+
 class Board
   def initialize
     @columns = Array.new(7) { Array.new(6) }
@@ -11,5 +15,17 @@ class Board
       first_free_index = column.find_index(nil)
       column[first_free_index] = disc
     end
+  end
+
+  def to_s
+    representation = ''
+    5.downto(0) do |index|
+      @columns.each do |column|
+        cell = column[index].nil? ? Tokens::EMPTY : column[index]
+        representation << " #{cell}"
+      end
+      representation << "\n"
+    end
+    representation
   end
 end
---
+++ lib/tokens.rb
@@ -0,0 +1,5 @@
+module Tokens
+  EMPTY = "\u25CB"
+  YELLOW = "\e[93m\u25CF\e[m"
+  RED = "\e[31m\u25CF\e[m"
+end
