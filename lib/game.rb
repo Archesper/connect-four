009192e7da649434e30699958d9fb9168c758228
Archesper <pk4nde@gmail.com>
2021-09-23 00:35:55 +0100

    Refactor horizontal win check to use Disc objects, implement vertical win check

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require './lib/board'
+require 'pry-byebug'
 
 class Game
   def initialize(player1, player2)
@@ -21,7 +22,7 @@ class Game
   end
 
   def player_input
-    puts 'Pick the column you wanna drop a disk in ( From 1 to 7 )'
+    puts 'Pick the column you wanna drop a disc in ( From 1 to 7 )'
     gets.chomp
   end
 
@@ -31,5 +32,54 @@ class Game
 
   def switch_current_player
     @current_player = @players.find { |player| player != @current_player }
+  end
+
+  def over?
+    horizontal_connect_four || vertical_connect_four
+  end
+
+  private
+
+  # These methods start their checks from the last disc that was pushed, as that is the disc that must have caused the connect four
+  def horizontal_connect_four
+    last_disc = @board.last_disc
+    row_to_check = @board.row(last_disc.row_index)
+    connected_discs = []
+    # Add discs to the right of last disc until different disc, empty cell or board edge is hit
+    current_disc = row_to_check[last_disc.column_index + 1]
+    until current_disc != last_disc
+      connected_discs << current_disc
+      current_disc = row_to_check[current_disc.column_index + 1]
+    end
+    # Add discs to the left of last disc until different disc, empty cell or board edge is hit
+    current_disc = row_to_check[last_disc.column_index - 1]
+    until current_disc != last_disc
+      connected_discs << current_disc
+      current_disc = row_to_check[current_disc.column_index - 1]
+    end
+    # Add last disc
+    connected_discs << last_disc
+    connected_discs.length == 4
+  end
+
+  def vertical_connect_four
+    last_disc = @board.last_disc
+    column_to_check = @board.column(last_disc.column_index)
+    connected_discs = []
+    # Add discs above last disc until different disc, empty cell or board edge is hit
+    current_disc = column_to_check[last_disc.row_index + 1]
+    until current_disc != last_disc
+      connected_discs << current_disc
+      current_disc = column_to_check[current_disc.row_index + 1]
+    end
+    # Add discs to the left of last disc until different disc, empty cell or board edge is hit
+    current_disc = column_to_check[last_disc.row_index - 1]
+    until current_disc != last_disc
+      connected_discs << current_disc
+      current_disc = column_to_check[current_disc.row_index - 1]
+    end
+    # Add last disc
+    connected_discs << last_disc
+    connected_discs.length == 4
   end
 end
