809ed5b5e55d09afe3c406394ec9124d426ad860
Archesper <pk4nde@gmail.com>
2021-09-23 17:29:18 +0100

    Add diagonal checking

--- lib/game.rb
+++ lib/game.rb
@@ -37,13 +37,13 @@ class Game
   def over?
     return false if @board.last_disc.nil?
 
-    horizontal_connect_four || vertical_connect_four
+    horizontal_connect_four? || vertical_connect_four? || positive_slope_diagonal_connect_four? || negative_slope_diagonal_connect_four?
   end
 
   private
 
   # These methods start their checks from the last disc that was pushed, as that is the disc that must have caused the connect four
-  def horizontal_connect_four
+  def horizontal_connect_four?
     last_disc = @board.last_disc
     row_to_check = @board.row(last_disc.row_index)
     connected_discs = []
@@ -64,7 +64,7 @@ class Game
     connected_discs.length == 4
   end
 
-  def vertical_connect_four
+  def vertical_connect_four?
     last_disc = @board.last_disc
     column_to_check = @board.column(last_disc.column_index)
     connected_discs = []
@@ -79,6 +79,46 @@ class Game
     until current_disc != last_disc
       connected_discs << current_disc
       current_disc = column_to_check[current_disc.row_index - 1]
+    end
+    # Add last disc
+    connected_discs << last_disc
+    connected_discs.length == 4
+  end
+
+  def positive_slope_diagonal_connect_four?
+    last_disc = @board.last_disc
+    connected_discs = []
+    # Add discs diagonally above until different disc, empty cell or board edge is hit
+    current_disc = @board.column(last_disc.column_index + 1)[last_disc.row_index + 1]
+    until current_disc != last_disc
+      connected_discs << current_disc
+      current_disc = @board.column(current_disc.column_index + 1)[current_disc.row_index + 1]
+    end
+    # Add discs diagonally below until different disc, empty cell or board edge is hit
+    current_disc = @board.column(last_disc.column_index - 1)[last_disc.row_index - 1]
+    until current_disc != last_disc
+      connected_discs << current_disc
+      current_disc = @board.column(current_disc.column_index - 1)[current_disc.row_index - 1]
+    end
+    # Add last disc
+    connected_discs << last_disc
+    connected_discs.length == 4
+  end
+
+  def negative_slope_diagonal_connect_four?
+    last_disc = @board.last_disc
+    connected_discs = []
+    # Add discs diagonally below until different disc, empty cell or board edge is hit
+    current_disc = @board.column(last_disc.column_index + 1)[last_disc.row_index - 1]
+    until current_disc != last_disc
+      connected_discs << current_disc
+      current_disc = @board.column(current_disc.column_index + 1)[current_disc.row_index - 1]
+    end
+    # Add discs diagonally above until different disc, empty cell or board edge is hit
+    current_disc = @board.column(last_disc.column_index - 1)[last_disc.row_index + 1]
+    until current_disc != last_disc
+      connected_discs << current_disc
+      current_disc = @board.column(current_disc.column_index - 1)[current_disc.row_index + 1]
     end
     # Add last disc
     connected_discs << last_disc
