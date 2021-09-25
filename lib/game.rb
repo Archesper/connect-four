ff75080bfd9880045332edd950ca0cb27843d9de
Archesper <pk4nde@gmail.com>
2021-09-25 21:37:47 +0100

    Implement playing against computer

--- lib/game.rb
+++ lib/game.rb
@@ -29,6 +29,7 @@ class Game
       puts @board
       player_turn
     end
+    puts board
     puts finish_message
   end
 
@@ -64,11 +65,10 @@ class Game
   end
 
   def self.game_setup
-    puts "\e[34mThis is Connect Four, a game whose goal is to form a horizontal, vertical, or diagonal line by aligning 4 disks of the same color.\e[m"
     names = player_names
     red_player = Player.new(names[:red], Display::RED_DISC, :red)
     yellow_player = Player.new(names[:yellow], Display::YELLOW_DISC, :yellow)
-    Game.new(red_player, yellow_player)
+    new(red_player, yellow_player)
   end
 
   private
@@ -168,4 +168,73 @@ class Game
     { red: red_player_name, yellow: yellow_player_name }
   end
   private_class_method :player_names
+end
+
+class GameAgainstComputer < Game
+  def initialize(human_token, computer_token)
+    @board = Board.new
+    @human_token = human_token
+    @computer_token = computer_token
+  end
+
+  def self.game_setup
+    puts 'Would you rather play with red disc or the yellow disc? (Input red, yellow, R, Y)'
+    color = gets.chomp.downcase
+    until %w[red r yellow y].include? color
+      puts "\e[33mPlease pick one of the specified colors. (red, yellow, R, Y)\e[m"
+      color = gets.chomp.downcase
+    end
+    case color
+    when 'red', 'r'
+      human_token = Display::RED_DISC
+      computer_token = Display::YELLOW_DISC
+    when 'yellow', 'y'
+      human_token = Display::YELLOW_DISC
+      computer_token = Display::RED_DISC
+    end
+    new(human_token, computer_token)
+  end
+
+  def play
+    turn = 0
+    until over?
+      puts @board
+      turn.even? ? player_turn : computer_turn
+      turn += 1
+    end
+    puts @board
+    puts finish_message
+  end
+
+  def player_turn
+    puts "It's your turn!"
+    input = nil
+    loop do
+      input = verify_input(player_input)
+      break unless input.nil? || input == :full
+
+      input.nil? ? puts("\e[33mInvalid input. Please pick a column between 1 and 7.\e[m") : puts("\e[33mTargetted column is full, please pick another.\e[m")
+    end
+    update_board(input)
+  end
+
+  def computer_turn
+    puts "It's the computer's turn."
+    column_to_push_to = @board.non_full_columns.sample
+    @board.push_disc(column_to_push_to, @computer_token)
+  end
+
+  def update_board(input)
+    @board.push_disc(input.to_i, @human_token)
+  end
+
+  def finish_message
+    if @board.count_disc(nil).zero?
+      "Game over! It's a tie."
+    elsif @human_token == @board.last_disc.token
+      "\e[92mGame over! You win!\e[m"
+    else
+      "\e[33mGame over! Computer wins.\e[m"
+    end
+  end
 end
